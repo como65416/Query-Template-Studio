e1575e5bb81439c5a70e8d5bf3172a4f61e4c974
comoco <como65416@gmail.com>
2021-08-28 22:57:05 +0800

    add DataTable component

---
+++ src/components/DataTable.vue
@@ -0,0 +1,52 @@
+<template>
+  <el-table
+    :data="datas"
+    size="mini"
+    :header-cell-style="{background:'#F3F4F7',color:'#555'}"
+    border>
+    <el-table-column
+      v-for="title in titles"
+      :key="title"
+      :width="150"
+      :prop="title"
+      :label="title">
+      <template v-slot="scope">
+        {{ scope.row[title] }}
+        <el-tag
+          v-if="scope.row[title] === null"
+          size="mini"
+          type="info"
+          effect="plain">
+          NULL
+        </el-tag>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'DataTable',
+  props: {
+    titles: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    },
+    datas: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    }
+  },
+  methods: {
+  }
+})
+</script>
+
+<style scoped>
+</style>
