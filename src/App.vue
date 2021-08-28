cc0225b13315353fe89b0b0e4da398bedf86456c
comoco <como65416@gmail.com>
2021-08-28 22:58:15 +0800

    update UI

--- src/App.vue
+++ src/App.vue
@@ -8,20 +8,87 @@
       />
     </el-aside>
     <el-main>
+      <div v-for='config in scriptConfigs' :key='config.id'>
+        <VariableField v-model:variables="config.variables" />
+        <center>
+          <el-button
+            type="success"
+            size="mini"
+            icon="el-icon-search"
+          >Query</el-button>
+        </center>
+        <div v-for="script in config.scripts" :key="script.sql">
+          <h3>{{ script.name }}</h3>
+          <pre>{{ generateBindedSQL(script.sql, config.variables) }}</pre>
+          <DataTable :titles="script.titles" :data="script.datas"/>
+        </div>
+      </div>
     </el-main>
   </el-container>
 </template>
 
 <script lang="ts">
+import { SqlUtil } from '@/libs'
 import { defineComponent } from 'vue'
-import Sidebar from './components/Sidebar.vue'
+import { DataTable, Sidebar, VariableField } from '@/components'
+import { ScriptConfig, VariableData } from '@/types'
+
+declare interface BaseComponentData {
+  scriptConfigs: ScriptConfig[]
+}
 
 export default defineComponent({
   name: 'App',
+  data (): BaseComponentData {
+    return {
+      scriptConfigs: [
+        {
+          id: 1,
+          variables: [
+            {
+              keyword: '$age',
+              name: '年齡',
+              type: Number,
+              value: null
+            },
+            {
+              keyword: '$name',
+              name: '姓名',
+              type: String,
+              value: null
+            }
+          ],
+          scripts: [
+            {
+              name: '查詢年齡',
+              sql: 'SELECT * FROM Student WHERE age = $age',
+              result: {
+                titles: [],
+                datas: []
+              }
+            },
+            {
+              name: '查詢指定名字',
+              sql: 'SELECT * FROM Student WHERE name = $name',
+              result: {
+                titles: [],
+                datas: []
+              }
+            }
+          ]
+        }
+      ]
+    }
+  },
   components: {
-    Sidebar
+    Sidebar,
+    DataTable,
+    VariableField
   },
   methods: {
+    generateBindedSQL (sql: string, variables: VariableData[]) {
+      return SqlUtil.generateBindedSQL(sql, variables)
+    },
     optionClicked (key :string, keyPath :string) {
       console.log('parent', key, keyPath)
     },
---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+import DataTable from './DataTable.vue'
+import Sidebar from './Sidebar.vue'
+import VariableField from './VariableField.vue'
+
+export {
+  DataTable,
+  Sidebar,
+  VariableField
+}
