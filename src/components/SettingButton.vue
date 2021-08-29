928ead283a6a519ce229219eb43e5ac5dcc96c68
comoco <como65416@gmail.com>
2021-08-29 13:16:47 +0800

    add SettingButton

---
+++ src/components/SettingButton.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="floating-buttons">
+    <el-button
+      :icon="isExpanded ? 'el-icon-arrow-left' : 'el-icon-arrow-right'"
+      size="mini"
+      @click="isExpanded = !isExpanded"
+      circle>
+    </el-button>
+
+    <el-button
+      v-show="isExpanded"
+      type="success"
+      icon="el-icon-folder-add"
+      size="medium"
+      @click="handleCreateFolderClick"
+      circle>
+    </el-button>
+    <el-button
+      v-show="isExpanded"
+      type="primary"
+      icon="el-icon-document-add"
+      size="medium"
+      @click="handleCreateScriptClick"
+      circle>
+    </el-button>
+    <el-button
+      v-show="isExpanded"
+      type="warning"
+      icon="el-icon-setting"
+      size="medium"
+      @click="handleSettingClick"
+      circle>
+    </el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'SettingButton',
+  data () {
+    return {
+      isExpanded: false
+    }
+  },
+  props: {
+  },
+  methods: {
+    handleCreateFolderClick () {
+      this.$emit('onCreateFolderClick')
+    },
+    handleCreateScriptClick () {
+      this.$emit('onCreateScriptClick')
+    },
+    handleSettingClick () {
+      this.$emit('onSettingClick')
+    }
+  }
+})
+</script>
+
+<style scoped>
+  .floating-buttons {
+    position: fixed;
+    left: 10px;
+    bottom: 10px;
+  }
+</style>
