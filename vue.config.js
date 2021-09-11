b8ac53ff37b6619f4198424ccb5fe3ed881db949
comoco <como65416@gmail.com>
2021-09-11 22:20:18 +0800

    add icon

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   configureWebpack: {
     target: 'electron-renderer'
+  },
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        win: {
+          icon: 'build/icons/icon.ico'
+        },
+        mac: {
+          icon: 'build/icons/icon.png'
+        }
+      }
+    }
   }
 }
