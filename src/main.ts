b6834a116c5fa6fdcecf1ceaeed7ab6164099935
comoco <como65416@gmail.com>
2021-09-18 10:15:14 +0800

    upgrade vue and element ui version

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
-import 'element-plus/lib/theme-chalk/index.css'
 import 'ag-grid-community/dist/styles/ag-grid.css'
 import 'ag-grid-community/dist/styles/ag-theme-balham.css'
 import 'ace-builds/webpack-resolver'
 import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
